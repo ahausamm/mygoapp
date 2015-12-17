bc06648b9b0e8d8aaba8101401468745e811b26b
Adrian Hausammann <adhausammann@gmx.ch>
2015-12-18 00:38:09 +0100

    Changes Content

--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import(
 "fmt"
 "net/http"
-"runtime"
 )
  
 func indexHandler( w http.ResponseWriter, r *http.Request){
-fmt.Fprintf(w, "hello world, I'm running on %s with an %s CPU and this is a new content ", runtime.GOOS,runtime.GOARCH)
+fmt.Fprintf(w, "hello world, this is content of adrians simple website ")
  
 }
  
